76b09145d0dfd446f8867ee9b475ed10b7d8f7be
Derix Venegas <derix.dv19@gmail.com>
2023-10-12 11:43:46 -0400

    Finalizado

---
+++ src/assets/places.js
@@ -0,0 +1,43 @@
+
+export const places = [
+    {
+        title:"Villa, Kemah Tinggi",
+        bedroom: "2 ",
+        size:"214 ",
+        price:"920",
+        score:"4,93",
+        imgUrl:"/place1.svg"
+    },
+    {
+        title:"Villa, Ziba Seminyak",
+        bedroom: "5 ",
+        size:"520 ",
+        price:"1100",
+        score:"4,89",
+        imgUrl:"/place2.svg"
+    },
+    {
+        title:"Villa, Kecamatan Ubud",
+        bedroom: "2 ",
+        size:"214 ",
+        price:"920",
+        score:"4,89",
+        imgUrl:"/place3.svg"
+    },
+{
+    title:"Villa, Canggu",
+    bedroom: "5 ",
+    size:"600 ",
+    price:"400",
+    score:"4,82",
+    imgUrl:"/place4.svg"
+    },
+{
+    title:"Villa, Kerobocan Kelod",
+    bedroom: "4 ",
+    size:"1000 ",
+    price:"750",
+    score:"4,80",
+    imgUrl:"/place5.svg"
+    }
+]
