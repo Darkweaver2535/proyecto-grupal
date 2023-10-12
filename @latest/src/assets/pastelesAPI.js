ef128b46d31fc4a8dab4030360f5e6186a915c89
Darkweaver <albasantiagoe@hotmail.com>
2023-10-12 11:13:18 -0400

    API

---
+++ @latest/src/assets/pastelesAPI.js
@@ -0,0 +1,72 @@
+export const cakes = [
+  {
+    title: "Chocolate Cake",
+    description:
+      "Indulge in the rich and decadent flavors of a chocolate cake. Perfect for satisfying your sweet cravings and celebrating special occasions.",
+    imgUrl: "/chocolate-cake.svg",
+    theme: "brown",
+  },
+  {
+    title: "Strawberry Cake",
+    description:
+      "Enjoy the delightful taste of a strawberry cake, filled with fresh strawberries and a light, fruity frosting. Ideal for a refreshing treat.",
+    imgUrl: "/strawberry-cake.svg",
+    theme: "pink",
+  },
+  {
+    title: "Red Velvet Cake",
+    description:
+      "Experience the luxurious taste of a red velvet cake, known for its vibrant red color and creamy cream cheese frosting. Perfect for a touch of elegance.",
+    imgUrl: "/red-velvet-cake.svg",
+    theme: "crimson",
+  },
+  {
+    title: "Cheesecake",
+    description:
+      "Savor the creamy goodness of a cheesecake, available in various flavors from classic New York style to fruity variations. A dessert lover's delight.",
+    imgUrl: "/cheesecake.svg",
+    theme: "cream",
+  },
+  {
+    title: "Fruit Tart",
+    description:
+      "Delight in the combination of fresh fruits atop a buttery pastry crust. Fruit tarts offer a burst of natural sweetness and a touch of tartness.",
+    imgUrl: "/fruit-tart.svg",
+    theme: "yellow",
+  },
+  {
+    title: "Cupcakes",
+    description:
+      "Treat yourself to cute and delicious cupcakes, available in various flavors and topped with colorful frosting and sprinkles. Perfect for any occasion.",
+    imgUrl: "/cupcakes.svg",
+    theme: "pastel",
+  },
+  {
+    title: "Tiramisu",
+    description:
+      "Experience the classic Italian dessert, Tiramisu, with layers of coffee-soaked ladyfingers and mascarpone cheese. A coffee lover's dream.",
+    imgUrl: "/tiramisu.svg",
+    theme: "espresso",
+  },
+  {
+    title: "Pecan Pie",
+    description:
+      "Enjoy the sweet and nutty flavor of a pecan pie, made with a buttery crust and a filling of pecans, sugar, and syrup. A traditional favorite.",
+    imgUrl: "/pecan-pie.svg",
+    theme: "golden-brown",
+  },
+  {
+    title: "Lemon Cake",
+    description:
+      "Brighten your day with a zesty lemon cake, featuring a tangy lemon flavor and a light, lemony frosting. Perfect for a refreshing dessert.",
+    imgUrl: "/lemon-cake.svg",
+    theme: "yellow",
+  },
+  {
+    title: "Carrot Cake",
+    description:
+      "Satisfy your taste buds with a moist and spiced carrot cake, typically topped with cream cheese frosting and a sprinkle of nuts. A classic choice.",
+    imgUrl: "/carrot-cake.svg",
+    theme: "orange",
+  },
+];
